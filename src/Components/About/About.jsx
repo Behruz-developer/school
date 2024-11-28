217e2d767de646d7cd01606d7607b09453a63a3c
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2024-11-28 11:13:49 +0500

    update

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <div className='about'>
             <div className="container">
                 <div className="about_title_box">
-                    <h2 className="title"> <span>“HAMRAQULOV MAKTAB” </span> <br /> HAQIDA</h2>
+                    <h2 className="title"> <span>“HAMRAKULOV MAKTABI” </span> <br /> HAQIDA</h2>
                     <img src={corner} alt="" />
                 </div>
                 <p className="about_text">
